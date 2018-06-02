ef08c22473097f12ed6e0db714f513e778e963ee
denismoroz1981 <denismoroz@mail.ru>
2018-06-02 18:54:49 +0300

    api class

--- backend/controllers/ApiuploadController.php
+++ backend/controllers/ApiuploadController.php
@@ -2,12 +2,94 @@
 
 namespace backend\controllers;
 
+use common\models\Offers;
 use Yii;
 
-define("API_KEY","tK5DsVHHZCjHlv81j9LiiQiiUWNryTQ1kmiXc5pZ");
+//define("API_KEY","tK5DsVHHZCjHlv81j9LiiQiiUWNryTQ1kmiXc5pZ");
 
 //function for pretty printing of JSON format
 
+class ApiUpload {
+
+    private $path;
+    private $params;
+    private $button;
+    private $items_per_request;
+    private $items_per_day;
+    private $title;
+    private $isCommon;
+    private $apiDb = [
+        "btn_cat"=>[
+            "path"=>"https://developers.ria.com/dom/options?",
+            "items_per_request"=>NULL,
+            "items_per_day"=>8000,
+            "title" =>"Realty categories from DOM.RIA",
+            "isCommon"=>true
+        ],
+        "btn_id"=>[
+            "path"=>"https://developers.ria.com/dom/info/",
+            "items_per_request"=>1,
+            "items_per_day"=>8000,
+            "title" =>"Realty categories from DOM.RIA",
+            "isCommon"=>false
+
+        ],
+        "btn_offer"=>[
+            "path"=>"https://developers.ria.com/dom/search?",
+            "items_per_request"=>100,
+            "items_per_day"=>8000,
+            "title" =>"Realty categories from DOM.RIA",
+            "isCommon"=>true
+        ]
+    ];
+
+    public function __construct(array $params)
+    {
+        $this->params = $params;
+        $this->button = $this->params["button"];
+        $this->path = $this->apiDb[$this->button]["path"];
+        $this->items_per_request = $this->apiDb[$this->button]["items_per_request"];
+        $this->items_per_day = $this->apiDb[$this->button]["items_per_day"];
+        $this->title = $this->apiDb[$this->button]["title"];
+        $this->isCommon = $this->apiDb[$this->button]["isCommon"];
+    }
+
+
+
+    private function getDataCommon() {
+
+        foreach ($this->params as $k => $v){
+            $this->path .="$k"."="."$v"."&";
+        }
+        $data = json_decode(file_get_contents($this->path),true);
+        if (empty($data["count"])) {return ["uploadedlist"=>$data,"title"=>$this->title];}
+        if(count($data["items"])>=$data["count"]) {
+            return ["uploadedlist" => $data, "title" => $this->title];
+        }
+        if(count($data["items"])<$data["count"]) {
+            $iter = ceil($data["count"]/$this->items_per_request);
+            for ($i=1;$i<=$iter;$i++) {
+                $path_iter=$this->path.="page=".$i;
+
+            $data_iter = json_decode(file_get_contents($path_iter),true);
+            $data = array_push($data["items"],$data_iter["items"]);}
+            return ["uploadedlist" => $data, "title" => $this->title];
+        }
+    }
+
+    public function getData() {
+        if ($this->isCommon) { return $this->getDataCommon();}
+        if ($this->button == "btn_id") {
+            $path = "https://developers.ria.com/dom/info/".print_r($this->params["id"],1).
+                "?";
+            $data = json_decode(file_get_contents($path),true);
+            return $data;
+        }
+    }
+
+
+
+}
 
 
 
@@ -15,23 +97,12 @@ class ApiuploadController extends \yii\web\Controller
 {
     public function actionIndex()
     {
-        /*    $get_from_user = $_GET;
-            $path = "https://developers.ria.com/dom/options?";
-            foreach ($get_from_user as $k => $v){
-                $path .="$k"."="."$v"."&";
-
-
-            }
-            header('Content-Type: application/json');
-            echo prettyPrint(file_get_contents($path));
-
-           */
-
-        return $this->render('index', ["api_key" => API_KEY]);
+        return $this->render('index');
     }
 
-    public function actionUploadedlist() {
 
+
+    public function actionUploadedlist() {
 
 
         function toCsv($array) {
@@ -55,33 +126,47 @@ class ApiuploadController extends \yii\web\Controller
 
         }
 
+
         echo 'Uploaded json from RIA.DOM';
 
-        $request=Yii::$app->request;
+        /*
 
 
         $get_from_user = $request->get();
         if (!empty($get_from_user["btn_cat"])) {
             $title="Realty categories from DOM.RIA";
             $path = "https://developers.ria.com/dom/options?";
+            $ul = new ApiUpload($path,$get_from_user);
+            $uploadedlist = $ul->getData();
         }
+
+        if (!empty($get_from_user["btn_id"])) {
+            $title="Info for offer with id -".print_r($get_from_user["id"],1)."- from DOM.RIA";
+            $path = "https://developers.ria.com/dom/info/".print_r($get_from_user["id"],1).
+                "?";
+            $ul = new ApiUpload($path,$get_from_user);
+            $uploadedlist = $ul->getData();
+        }
+
         if (!empty($get_from_user["btn_offer"])) {
             $title="Realty offer ids from DOM.RIA";
             $path = "https://developers.ria.com/dom/search?";
-        }
-        if (!empty($get_from_user["btn_id"])) {
-            $title="Info for offer with id -".print_r($get_from_user["id"],1)."- from DOM.RIA";
-            $path = "https://developers.ria.com/dom/info/".print_r($get_from_user["id"],1)."?";
-        }
-        foreach ($get_from_user as $k => $v){
-            $path .="$k"."="."$v"."&";
+            $ul = new ApiUpload($path,$get_from_user);
+            $uploadedlist = $ul->getData();
+        }*/
+        $request=Yii::$app->request->get();
+        $ul = new ApiUpload($request);
+        $uploadedlist=$ul->getData();
 
+        return $this->render('index',
+            $uploadedlist);
 
-        }
+    }
+    public function actionSavetodb() { // save data uploaded from API to DB
+        //$offers=Offers::find();
+        $model = new Offers();
+        //$model->id = 1
 
-        $uploadedlist = json_decode(file_get_contents($path),true);
-        return $this->render('uploadedlist',
-            ['uploadedlist'=>$uploadedlist,"title"=>$title]);
 
     }
 }
--- backend/views/apiupload/index.php
+++ backend/views/apiupload/index.php
@@ -3,8 +3,26 @@
 //use \yii\widgets\ActiveForm;
 use yii\helpers\Html;
 
+$api_key='tK5DsVHHZCjHlv81j9LiiQiiUWNryTQ1kmiXc5pZ';
 ?>
 <h1>DOM.RIA API upload page</h1>
+<style>
+    hr {
+        display: block;
+        margin-top: 0.5em;
+        margin-bottom: 0.5em;
+        margin-left: auto;
+        margin-right: auto;
+        border-style: inset;
+        border-width: 1px;
+    }
+    td {
+        padding: 15px;
+    }
+    
+
+</style>
+
 
 <p><b>developer's api_key:</b> <i><?= print_r($api_key) ?></i></p>
 
@@ -25,6 +43,9 @@ echo Html::label('api_key:');
 echo Html::input('text','api_key',$api_key_updated,['size'=>40]);
 echo '<br>';
 echo '<br>';
+echo '<table cellpadding="5px">';
+echo '<tr>';
+echo '<td>';
 echo Html::label('category:');
 echo Html::input('text','category',$category,['size'=>2]);
 echo Html::label('realty type:');
@@ -32,21 +53,37 @@ echo Html::input('text','realty_type',$realty_type,['size'=>2]);
 echo Html::label('operation type:');
 echo Html::input('text','operation_type',$operation_type,['size'=>2]);
 echo '<br>';
-echo Html::submitButton('Show offer\'s categories',['name'=>'btn_cat','value'=>'categories']);
-echo '<br>';
-echo '<br>';
+echo Html::submitButton('Show offer\'s categories',['name'=>'button','value'=>'btn_cat']);
+echo '</td>';
+echo '<td>';
+//echo '<br>';
+//echo '<br>';
 echo Html::label('state id:');
 echo Html::input('text','state_id',$state_id,['size'=>2]);
 echo Html::label('city id:');
 echo Html::input('text','city_id',$city_id,['size'=>2]);
 echo '<br>';
-echo Html::submitButton('Upload offers',['name'=>'btn_offer','value'=>'offers']);
-echo '<br>';
-echo '<br>';
+echo Html::submitButton('Upload offers',['name'=>'button','value'=>'btn_offer']);
+echo '</td>';
+echo '<td>';
+//echo '<br>';
+//echo '<br>';
 echo Html::label('offer id:');
 echo Html::input('text','id',$id,['size'=>10]);
 echo '<br>';
-echo Html::submitButton('Info by id',['name'=>'btn_id','value'=>'id']);
+echo Html::submitButton('Info by id',['name'=>'button','value'=>'btn_id']);
+echo '</td>';
+echo '</tr>';
+echo '</table>';
 echo Html::endForm();
 
+//echo '<br>';
+echo '<hr>';
+if (empty($title)) {$title='';}
+if (empty($uploadedlist)) {$uploadedlist='';}
+echo '<h3>'.print_r($title,1).'</h3>';
+//echo "<h1>Uploaded real estate features from DOM.RIA api</h1><br />";
+echo '<br>';
+Kint::dump($uploadedlist);
 ?>
+
