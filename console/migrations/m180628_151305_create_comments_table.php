65c5885c10602f59578fd6e69a85eb139a7ac33e
denismoroz1981 <denismoroz@mail.ru>
2018-06-28 18:32:21 +0300

    migration for comments table added

---
+++ console/migrations/m180628_151305_create_comments_table.php
@@ -0,0 +1,64 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * Handles the creation of table `comments`.
+ */
+class m180628_151305_create_comments_table extends Migration
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function safeUp()
+    {
+        $this->createTable('comments', [
+            'id' => $this->primaryKey(),
+            'offers_id' => 'integer',
+            'comment' => 'string',
+            'user' => 'string',
+            'isapproved' => 'integer',
+            'created_at' => $this->dateTime().' DEFAULT NOW()',
+        ],
+            'ENGINE=InnoDB', 'CHARACTER SET=utf8','COLLATE=utf8_general_ci'
+        );
+
+        // creates index for column `offers_id`
+        $this->createIndex(
+            'idx-comments_id',
+            'comments',
+            'offers_id'
+        );
+
+        // add foreign key for table `comments`
+        $this->addForeignKey(
+            'fk-comments_id',
+            'comments',
+            'offers_id',
+            'offers',
+            'realty_id',
+            'CASCADE'
+        );
+
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function safeDown()
+    {
+        // drops foreign key for table `comments`
+        $this->dropForeignKey(
+            'fk-comments_id',
+            'comments'
+        );
+
+        // drops index for column `offers_id`
+        $this->dropIndex(
+            'idx-comments_id',
+            'comments'
+        );
+
+        $this->dropTable('comments');
+    }
+}
