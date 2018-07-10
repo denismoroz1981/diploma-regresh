5c28015bf0cb4ffe6e31b888d440e34230ab2962
denismoroz1981 <denismoroz@mail.ru>
2018-07-10 20:39:57 +0300

    final commit 2

---
+++ frontend/web/index.php
@@ -0,0 +1,19 @@
+<?php
+//echo var_export(session_save_path());
+//ini_set('session.save_path', '0;640;' . session_save_path());
+//defined('YII_DEBUG') or define('YII_DEBUG', true);
+//defined('YII_ENV') or define('YII_ENV', 'dev');
+
+require __DIR__ . '/../../vendor/autoload.php';
+require __DIR__ . '/../../vendor/yiisoft/yii2/Yii.php';
+require __DIR__ . '/../../common/config/bootstrap.php';
+require __DIR__ . '/../config/bootstrap.php';
+
+$config = yii\helpers\ArrayHelper::merge(
+    require __DIR__ . '/../../common/config/main.php',
+    require __DIR__ . '/../../common/config/main-local.php',
+    require __DIR__ . '/../config/main.php',
+    require __DIR__ . '/../config/main-local.php'
+);
+
+(new yii\web\Application($config))->run();
